505a46e388cb5e17f8e29171bdf29f3bc9fbab2e
olisaemeka111 <olisa.arinze1@aol.com>
2024-08-12 20:45:51 +0100

    Update all the dependencies and ENV

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,23 @@
-
 # Use an official Python runtime as a parent image
-FROM python:3.9-slim
+FROM python:3.7-slim
 
-# Set the working directory in the container
-WORKDIR /usr/src/app
+# Set environment varibles
 
-# Copy the current directory contents into the container at /usr/src/app
-COPY . .
+ENV FLASK_APP=app.py
+ENV FLASK_RUN_HOST=0.0.0.0
+
+# Set work directory
+WORKDIR /app
 
 # Install any needed packages specified in requirements.txt
+COPY requirements.txt /app/requirements.txt
 RUN pip install --no-cache-dir -r requirements.txt
+
+# Copy the current directory contents into the container at /app
+COPY . /app
 
 # Make port 5000 available to the world outside this container
 EXPOSE 5000
 
-# Run app.py when the container launches
-CMD ["python", "app.py"]
+# Run the application when the container launches
+CMD ["flask", "run"]
--- app.py
+++ app.py
@@ -1,8 +1,27 @@
-
 from flask import Flask, request, jsonify
 import hashlib
 
 app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def home_page():
+    # Use a different function name
+    return '''
+   <!doctype html>
+    <html>
+    <body style="background-image: url('/static/bubble-float.gif'); background-size: cover; height: 100vh; color: white;">
+        <form method=POST enctype=multipart/form-data action="/upload" style="padding-top: 20px;">
+            <input type=file name=file>
+            <input type=submit value=Upload>
+        </form>
+        <footer style="position: fixed; left: 0; bottom: 0; width: 100%; text-align: center; background-color: rgba(0, 0, 0, 0.7); color: white;">
+            <p>Designed by Olisa Arinze  August 2024</p>
+        </footer>
+    </body>
+    </html>
+    '''
+
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
@@ -18,6 +37,7 @@ def upload_file():
     md5_hash = hashlib.md5(file_contents).hexdigest()
 
     return jsonify({'filename': file.filename, 'md5_hash': md5_hash})
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
